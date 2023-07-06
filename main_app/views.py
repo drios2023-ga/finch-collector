70fa83c4dd5d6d99aee3919fb7d93eaa3f035642
drios2023-ga <drios111-general@yahoo.com>
2023-07-06 12:07:09 -0500

    added model

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-finches = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+# Import the Cat Model
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -12,5 +10,17 @@ def home(request):
 def about(request):
     return render(request, 'about.html');  
 
-def finches_index(request):      
-    return render(request, 'finches/index.html', {'finches':finches})
+# def finches_index(request):      
+#     return render(request, 'finches/index.html', {'finches':finches})
+
+def finches_index(request):
+    finches = Finch.objects.all() # Retrieve all finches
+    return render(request, 'finches/index.html', 
+    { 
+        'finches':finches 
+    }
+)
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
